b12bcb83527ee72896e518f632eed09581925263
Jezreel Barbosa <jezreel.barbosa.42@gmail.com>
2021-01-07 09:53:15 -0400

    Implemented Networking Framework

---
+++ GroceryListProject/GroceryListProjectTests/GroceryListProjectTests.swift
@@ -0,0 +1,33 @@
+//
+//  GroceryListProjectTests.swift
+//  GroceryListProjectTests
+//
+//  Created by Jezreel Barbosa on 07/01/21.
+//  Copyright © 2021 JezreelBarbosa. All rights reserved.
+//
+
+import XCTest
+
+class GroceryListProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
