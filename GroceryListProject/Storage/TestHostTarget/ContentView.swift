12d76a2ffb09eed869a071ade99ab7cbab40ff60
Jezreel Barbosa <jezreel.barbosa.42@gmail.com>
2021-01-07 10:00:13 -0400

    Implemented Storage Framework

---
+++ GroceryListProject/Storage/TestHostTarget/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  TestHostTarget
+//
+//  Created by Jezreel Barbosa on 07/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
