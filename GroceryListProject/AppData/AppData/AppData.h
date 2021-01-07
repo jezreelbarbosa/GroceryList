217acd8d346385efdd70f1e140b508c17d68a176
Jezreel Barbosa <jezreel.barbosa.42@gmail.com>
2021-01-07 10:02:52 -0400

    Implemented AppDataFramework

---
+++ GroceryListProject/AppData/AppData/AppData.h
@@ -0,0 +1,18 @@
+//
+//  AppData.h
+//  AppData
+//
+//  Created by Jezreel Barbosa on 07/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppData.
+FOUNDATION_EXPORT double AppDataVersionNumber;
+
+//! Project version string for AppData.
+FOUNDATION_EXPORT const unsigned char AppDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppData/PublicHeader.h>
+
+
